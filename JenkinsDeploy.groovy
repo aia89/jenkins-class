f4cb274debae9955656fd8a6f95c73983c79dee5
aia89 <aiadosbaeva@gmail.com>
2020-09-10 19:10:18 -0500

    environments with params added

---
+++ JenkinsDeploy.groovy
@@ -0,0 +1,22 @@
+properties([
+    [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], 
+    parameters([
+        booleanParam(defaultValue: false, description: 'Please select to apply all changes to the environment ', name: 'applyChanges'), 
+        booleanParam(defaultValue: false, description: 'Please select to destroy all changes to the environment ', name: 'destroyChanges'), 
+        string(defaultValue: '', description: 'Please provide the docker to deploy', name: 'selectedDockerImage', trim: true), 
+        choice(choices: ['dev', 'qa', 'stage', 'prod'], description: 'Please provide the environment to deploy', name: 'environment')
+        ])
+        ])
+
+
+
+println(
+    """
+    Apply chnages: ${params.applyChanges}
+    Destroy chnages: ${params.destroyChanges}
+    Docker image: ${params.selectedDockerImage}
+    Environment: ${params.environment}
+
+
+    """
+)
