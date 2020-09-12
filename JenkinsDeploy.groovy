71f5a1feef53417c3e39b37cd6780747cd259c7b
aia89 <aiadosbaeva@gmail.com>
2020-09-12 16:10:57 -0500

    added stages to deploy job

--- JenkinsDeploy.groovy
+++ JenkinsDeploy.groovy
@@ -7,7 +7,64 @@ properties([
         choice(choices: ['dev', 'qa', 'stage', 'prod'], description: 'Please provide the environment to deploy ', name: 'environment')
         ])
         ])
- println(
+ 
+
+//unique name for the pod
+def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
+def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers:
+        - name: fuchicorptools
+          image: fuchicorp/buildtools
+          imagePullPolicy: Always
+          command:
+          - cat
+          tty: true
+        
+          
+        serviceAccountName: default
+        securityContext:
+          runAsUser: 0
+          fsGroup: 0
+        volumes:
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
+    """
+    podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
+      node(k8slabel) {
+
+        stage("Pull SCM") {
+            git 'https://github.com/aia89/jenkins-class.git'
+
+        }
+        
+        stage("Apply/Plan") {
+            container("fuchicorptools") {
+                sh 'kubectl version'
+            }
+        }
+      }
+    }
+
+
+
+println(
     """
     Apply changes: ${params.applyChanges}
     Destroy changes: ${params.destroyChanges}
@@ -15,3 +72,10 @@ properties([
     Environment: ${params.environment}
     """
  )
+
+
+
+
+
+
+
